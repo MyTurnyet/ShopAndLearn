9499748fb85c9d0e5b9f70da8eadc9b122b911e8
paige.watson <paige.watson@salesforce.com>
2024-12-04 11:47:19 -0800

    make this a springboot application update to use Kotlin-style gradle

---
+++ src/test/java/com/softwareascraft/shopandlearn/Inventory/ShoppingCartTests.java
@@ -0,0 +1,14 @@
+package com.softwareascraft.shopandlearn.Inventory;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Tag("Unit")
+public class ShoppingCartTests {
+    @Test
+    void testShoppingCart() {
+        assertThat(false).isFalse();
+    }
+}
